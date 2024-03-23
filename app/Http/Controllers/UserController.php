e508bb4335b0a440730410b456ec0ffd99de45d2
balqozz <cooleyahacc@gmail.com>
2024-03-24 00:20:29 +0700

    JS4 prac-2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,21 +65,64 @@ class UserController extends Controller
 
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::firstOrNew(
-            [
-                // 'username' => 'manager',
-                // 'nama' => 'Manager',
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         // 'username' => 'manager',
+        //         // 'nama' => 'Manager',
 
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash:make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+
+        // return view('user', ['data' => $user]);
+
+    // JS4 prac-2.5
+        // $user = UserModel::create([
+        //         'username' => 'manager55',
+        //         'nama' => 'Manager55',
+        //         'password' => Hash:make('12345'),
+        //         'level_id' => 2
+        //     ]);
+
+        // $user->username = 'manager56';
+
+        // $user->isDirty(); // true
+        // $user->isDirty ('username'); // true
+        // $user->isDirty('nama'); // false
+        // $user->isDirty(['nama', 'username']); // true
+
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean(['nama', 'username']); // false
+
+        // $user->save();
+
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+        // dd($user->isDirty());
+
+        $user = UserModel::create([
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash:make('12345'),
                 'level_id' => 2
-            ],
-        );
+            ]);
+
+        $user->username = 'manager12';
+
         $user->save();
 
-        return view('user', ['data' => $user]);
-
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        // $user->wasChanged(['nama', 'username']); // true
+        dd($user->wasChanged(['nama', 'username'])); // true
 
     }
 }
