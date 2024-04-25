d46f688236fd89e05c69ce84ca590c636deacb91
balqozz <cooleyahacc@gmail.com>
2024-04-26 02:12:04 +0700

    JS 6 - B. Validasi Pada Server

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -1,54 +1,60 @@
 @extends('layout.app')
 
+{{-- Customize layout sections --}}
+
 @section('subtitle', 'Kategori')
 @section('content_header_title', 'Kategori')
-@section('content_header_subtitle', 'Daftar Kategori')
+@section('content_header_subtitle', 'Create')
+
+{{-- Content body: main page content --}}
 
 @section('content')
     <div class="container">
         <div class="card card-primary">
             <div class="card-header">
-                <h3 class="card-title">Buat Kategori Baru</h3>
-                <form method="post" action="../kategori">
-                </div>
-                    <div class="card-body">
-                        @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->category->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                        <div class="form-group">
-
-                            <label for="kode_kategori">Kode Kategori</label>
-                            {{-- <input type="text" class="form-control" name="kodeKategori" id="KodeKategori"
-                                placeholder="Kode Kategori"> --}}
-
-                                <input type="text" class="form-control @error('kategori_kode') is-invalid @enderror" id="kategori_kode" name="kategori_kode"
-                                placeholder="Kode Kategori">
-                            @error('kategori_kode', 'category')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="nama_kategori">Nama Kategori</label>
-                            {{-- <input type="text" class="form-control" name="namaKategori" id="NamaKategori"
-                                placeholder="Nama Kategori"> --}}
-                                <input type="text" class="form-control @error('kategori_nama') is-invalid @enderror" id="kategori_nama" name="kategori_nama"
-                                placeholder="Nama Kategori">
-                            @error('kategori_nama', 'category')
-                                <div class="alert alert-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                    </div>
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Simpan</button>
-                    </div>
-                </form>
-
+                <h3 class="card-title">Buat kategori baru</h3>
             </div>
-        @endsection
+
+            <form method="post" action="../kategori">
+                {{ csrf_field() }}
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="katagori_kode">Kode Kategori</label>
+                    <input type="kategori_kode" type="text" class="form-control" id="kodeKategori" name="kodeKategori" placeholder="untuk makanan, contoh:MKN" class="@error('kategori_kode') is-invalid @enderror">
+                </div>
+                @error('kategori_kode')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+
+                <div class="form-group">
+                    <label for="namaKategori">Nama Kategori</label>
+                    <input type="text" class="form-control" id="namaKategori" name="namaKategori" placeholder="Nama">
+                </div>
+            </div>
+            
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+
+            {{-- <label for="kategori_kode">Kode kategori</label>
+
+            <input type="kategori_kode"
+                type="text"
+                name="kategori_kode"
+                class="@error('kategori_kode') is-invalid @enderror"> --}}
+
+            
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+
+        </form>
+        </div>
+    </div>
+@endsection
