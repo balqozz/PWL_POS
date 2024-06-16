b6c35e9286ed21ac94883f073f8cbddec81d93f5
balqozz <145304918+balqozz@users.noreply.github.com>
2024-06-16 22:27:49 +0700

    Jobsheet 11

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class BarangModel extends Model
 {
     use HasFactory;
+    protected $table = 'm_barang';
+    protected $primaryKey = 'barang_id';
+
+    protected $fillable = [ 'kategori_id','barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'image'];
+
+    public function kategori(): BelongsTo
+    {
+        return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+    public function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts'. $image)
+        );
+    }
 }
